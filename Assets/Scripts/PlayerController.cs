80ee7a0354f8c320582fb4e027920f0472c33df9
Troll1r <kiralpa1503@gmail.com>
2020-06-01 14:41:55 +0400

    Типа анимация

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody rigidbody in rigidbodies) {
+                rigidbody.isKinematic = false;
+            }
+        }
+        
+    }
+}
