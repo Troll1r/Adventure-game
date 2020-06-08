e9b75104dc97fa102007129cea12ed37a0cb724d
rm <r.molotov@yahoo.com>
2020-06-08 20:43:29 +0400

    gopnik animator, run mul

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region components
+    Animator animator;
+
+    #endregion components
+
+    private float curSpeed = 1;
+    public float RunMultiplier = 1;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -18,6 +25,13 @@ public class PlayerController : MonoBehaviour
                 rigidbody.isKinematic = false;
             }
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            curSpeed *= RunMultiplier;
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            curSpeed /= RunMultiplier;
+
+        Vector2 dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * curSpeed;
+        animator.SetFloat("Speed", dir.magnitude);
     }
 }
