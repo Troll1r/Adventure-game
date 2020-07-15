c73ba071d3e530a579775b643a715efb32568021
Troll1r <kiralpa1503@gmail.com>
2020-07-15 19:27:55 +0400

    Управление и камера от третьего лица

---
+++ Assets/Scripts/ChangeCam.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCam : MonoBehaviour
+{
+    public GameObject TPC;
+    public GameObject FPC;
+    public bool SwitchCam;
+    
+    void Start() 
+    {
+        SwitchCam = true;
+        
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp("c"))
+        {
+
+            if (SwitchCam == false)
+                SwitchCam = true;
+            else if (SwitchCam == true)
+                SwitchCam = false;
+        }
+
+        if (SwitchCam == false)
+        {
+            TPC.SetActive(true);
+            FPC.SetActive(false);
+        }
+        if (SwitchCam == true)
+        {
+            TPC.SetActive(false);
+            FPC.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+
+    private float curX = 0.0f;
+    private float curY = 0.0f;
+
+
+    public float Y_ANGLE_MIN = 10.0f;
+    public float Y_ANGLE_MAX = 80.0f;
+    public Transform target;
+    public float distance = 5.0f;
+    public float sensX = 4.0f;
+    public float sensY = 4.0f;
+
+    public int distanceMin = 4;
+    public int distanceMax = 10;
+
+    void Update()
+    {
+        curX += Input.GetAxis("Mouse X") * sensX;
+        curY += Input.GetAxis("Mouse Y") * sensY;
+        curY = Mathf.Clamp(curY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+    void LateUpdate()
+    {
+
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(curY, curX, 0);
+
+        transform.position = target.position + rotation * dir;
+        transform.LookAt(target.transform);
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+            distance -= Mathf.Sign(Input.GetAxis("Mouse ScrollWheel"));
+        distance = Mathf.Clamp(distance, distanceMin, distanceMax);
+    }
+}
