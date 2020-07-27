7ceeeac3eb04f09ceff52f3bd0aa2cf0582576e1
Yoonton <63466663+Yoonton@users.noreply.github.com>
2020-07-27 14:10:20 +0400

    do navmesha

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int range;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit);
+        Debug.DrawRay(ray.origin, ray.direction * range, Color.blue);
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Enemy"))
+                Debug.Log("Попадаю во врага!!!");
+            else
+                Debug.Log("Путь к врагу преграждает объект: " + hit.collider.name);
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+        }
+    }
+}
