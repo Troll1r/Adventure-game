cb1bf174ad8856d7fd825300236310c6d4df511f
Yoonton <63466663+Yoonton@users.noreply.github.com>
2021-03-14 14:01:42 +0400

    переделанный навмеш
    не работают анимации

--- Assets/Scripts/AICharacterControl.cs
+++ Assets/Scripts/AICharacterControl.cs
@@ -3,95 +3,47 @@ using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
 using System.Collections.Generic;
-
-namespace UnityStandardAssets.Characters.ThirdPerson
+public class AICharacterControl : MonoBehaviour
 {
-    [RequireComponent(typeof(NavMeshAgent))]
-    [RequireComponent(typeof(ThirdPersonCharacter))]
-    public class AICharacterControl : MonoBehaviour
+    public GameObject enemy;
+    private NavMeshAgent agent;
+    public Player character;
+    public int stopDistance = 2;
+    private bool isStop;
+    private bool trg = false;
+    private void Start()
     {
-        private NavMeshAgent agent;
-        public ThirdPersonCharacter character;
-
-        private GameObject target;
-        
-        public int rays = 8;
-        public int rayDistance = 30;
-        public int stopDistance = 2;
-        public float angle = 40;
-        public Vector3 offset;
-
-        private void Start()
+        agent = GetComponent<NavMeshAgent>();
+        character = FindObjectOfType<Player>();
+    }
+    private void FixedUpdate()
+    {
+       float distance = Vector3.Distance(character.transform.position, enemy.transform.position);
+       if (trg)
+       {
+       agent.SetDestination(character.transform.position);   
+       }
+       if (distance < stopDistance)
+       {
+           agent.isStopped = true;
+       }
+       if(distance > stopDistance)
+       {
+           agent.isStopped = false;
+       }
+        print(trg);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
         {
-            agent = GetComponent<NavMeshAgent>();
-            character = GetComponent<ThirdPersonCharacter>();
-
-            agent.updateRotation = false;
-            agent.updatePosition = true;
+            trg = true;
+            agent.isStopped = false;
         }
-        GameObject GetRaycast(Vector3 dir)
-        {
-            RaycastHit hit = new RaycastHit();
-            Vector3 pos = transform.position + offset;
-            if (Physics.Raycast(pos, dir, out hit, rayDistance))
-            {
-                if (hit.collider.GetComponent<PlayerController>())
-                {
-                    Debug.DrawLine(pos, hit.point, Color.green);
-                    return hit.collider.gameObject;
-                }
-                else
-                {
-                    Debug.DrawLine(pos, hit.point, Color.blue);
-                }
-            }
-            else
-            {
-                Debug.DrawRay(pos, dir * rayDistance, Color.red);
-            }
-            return null;
-        }
-        GameObject RayToScan()
-        {
-            GameObject result = null;
-            float j = 0;
-            for (int i = 0; i < rays; i++)
-            {
-                var x = Mathf.Sin(j);
-                var y = Mathf.Cos(j);
-                j += angle * Mathf.Deg2Rad / rays;
-                
-                Vector3 dir = transform.TransformDirection(new Vector3(x, 0, y));
-                if (x!=0) 
-                    dir = transform.TransformDirection(new Vector3(-x, 0, y));
-
-                if (result = GetRaycast(dir)) return result;
-            }
-            return result;
-        }
-        private void FixedUpdate()
-        {
-            if (target = RayToScan())
-            {
-                if (Vector3.Distance(transform.position, target.transform.position) < stopDistance)
-                {
-                    // stop
-                    agent.isStopped = true;
-                }
-                else
-                {
-                    agent.isStopped = false;
-                    agent.SetDestination(target.transform.position);
-                }
-            }
-            else
-            {
-                agent.isStopped = true;
-                // лучше задать рандомную точку и патрулирование
-            }
-
-            Vector3 dir = agent.isStopped ? Vector3.zero : agent.desiredVelocity;
-            character.Move(dir, false, false);
-        }        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        trg = false;
+        agent.isStopped = true;
     }
 }
